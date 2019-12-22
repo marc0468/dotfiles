2d041714dd946982b79133168ef9d6de4b64a571
marc0468 <iammarc.0468@gmail.com>
2019-12-22 10:57:15 +0900

    chocolateyのインストール要batファイルを登録

---
+++ chocolatey.bat
@@ -0,0 +1,17 @@
+@rem cmdプロンプトでchocolateyをインストール（要管理者権限）
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+@rem 一般ツール
+choco install -y googlechrome
+choco install -y mpc-hc
+choco install -y k-litecodecpackfull
+choco install -y 7zip.install
+
+@rem 開発環境
+choco install -y winmerge
+choco install -y git
+choco install -y sakuraeditor
+choco install -y vscode
+choco install -y arduino
+choco install -y androidstudio
+choco install -y heroku-cli
