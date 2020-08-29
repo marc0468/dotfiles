fb5a1f0d6704537aee9014a65bb3bbdb39f60a54
marc0468 <iammarc.0468@gmail.com>
2020-08-29 21:54:23 +0900

    windows用の設定を修正

--- windows/dotfilesLink.bat
+++ windows/dotfilesLink.bat
@@ -1,6 +1,3 @@
-mklink  %HOMEPATH%\.vimrc %HOMEPATH%\dev\dotfiles\.vimrc
-mklink  %HOMEPATH%\.minttyrc %HOMEPATH%\dev\dotfiles\.minttyrc
-mklink  %HOMEPATH%\.bash_profile %HOMEPATH%\dev\dotfiles\.bash_profile
-mklink  %HOMEPATH%\.gitignore_global %HOMEPATH%\dev\dotfiles\.gitignore_global
-mklink  %APPDATA%\sakura\sakura.ini %HOMEPATH%\dev\dotfiles\sakura.ini
-mklink  %HOMEPATH%\.gitconfig %HOMEPATH%\dev\dotfiles\.gitconfig
+mklink  %HOMEPATH%\.vimrc %CD%\.vimrc
+mklink  %HOMEPATH%\.gitignore_global %CD%\.gitignore_global
+mklink  %HOMEPATH%\.gitconfig %CD%\.gitconfig
