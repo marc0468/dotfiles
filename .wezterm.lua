cc00c2b91af0a2ecf952d355bba550267485ab17
Marco <iammarc.0468@gmail.com>
2024-02-05 16:20:31 +0900

    fix wezterm settings

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,32 +1,6 @@
 local wezterm = require 'wezterm'
-local config = {}
-
-if wezterm.config_builder then
-    config = wezterm.config_builder()
-end
-
-config.color_scheme = 'MaterialDesignColors'
-config.window_background_opacity = 0.85
-config.font = wezterm.font('RobotoMono Nerd Font Mono', {weight="Regular", stretch="Normal", style="Normal"})
-config.font_size = 12
-
--- ショートカットキー設定
-local act = wezterm.action
-config.keys = {
-    {
-        key = '\\',
-        mods = 'SHIFT|CMD',
-        action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-    },
-    {
-        key = '-',
-        mods = 'SHIFT|CMD',
-        action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-    },
-}
 
 wezterm.on('update-right-status', function(window, pane)
-  local date = wezterm.strftime '%m/%d (%a) %H:%M:%S   '
   local bat = ''
   for _, b in ipairs(wezterm.battery_info()) do
     local icon = ''
@@ -38,16 +12,27 @@ wezterm.on('update-right-status', function(window, pane)
       icon = '💡'
     elseif b.state == 'Empty' then
       icon = '🪫'
-    elseif b.state == 'Unknown' then
+    else
       icon = '❓'
     end
-
     bat = icon .. string.format('%.0f%%', b.state_of_charge * 100)
   end
 
   window:set_right_status(wezterm.format {
-    { Text = bat .. '  ' .. date },
+    { Text = bat .. '  ' .. wezterm.strftime '%m/%d (%a) %H:%M:%S   ' },
   })
 end)
 
-return config
+return {
+  color_scheme = 'MaterialDesignColors',
+  window_background_opacity = 0.85,
+  font = wezterm.font('RobotoMono Nerd Font Mono'),
+  font_size = 12,
+  show_update_window = false,
+  native_macos_fullscreen_mode = true,
+  keys = {
+    { key = '\\', mods = 'SHIFT|CMD', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+    { key = '-',  mods = 'SHIFT|CMD', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
+    { key = "f",  mods = "SHIFT|CMD", action = "ToggleFullScreen" },
+  }
+}
