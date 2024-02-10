b157ab9075945232a9746d9c84d008d0ee822b14
Marco <iammarc.0468@gmail.com>
2024-02-10 21:08:56 +0900

    update

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,1 @@
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\powerline.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\rudolfs-dark.omp.json" | Invoke-Expression
