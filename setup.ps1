c01f7f096a3d9288a4019cb9b47bf7ed610c904c
Marco <iammarc.0468@gmail.com>
2024-02-10 12:56:03 +0900

    update

--- setup.ps1
+++ setup.ps1
@@ -2,7 +2,5 @@ $path = (Convert-Path .)
 
 mkdir $HOME\Documents\PowerShell
 New-Item -ItemType SymbolicLink -Path $HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Value $path\PowerShell\Microsoft.PowerShell_profile.ps1
-mkdir $HOME\.config
-New-Item -ItemType SymbolicLink -Path $HOME\.config\starship.toml -Value $path\.config\starship.toml
 mkdir $HOME\.config\git
 New-Item -ItemType SymbolicLink -Path $HOME\.config\git\ignore -Value $path\.config\git\ignore
