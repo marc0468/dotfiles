14558824d9aa3574a25aa33ec19e6ac94b69539c
Marco <17253707+marc0468@users.noreply.github.com>
2025-06-01 15:42:10 +0900

    update

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,4 @@
 $path = (Convert-Path .)
 
-mkdir $HOME\Documents\PowerShell
-New-Item -ItemType SymbolicLink -Path $HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Value $path\PowerShell\Microsoft.PowerShell_profile.ps1
 mkdir $HOME\.config\git
 New-Item -ItemType SymbolicLink -Path $HOME\.config\git\ignore -Value $path\.config\git\ignore
